582b4efcf6490d81325f4e7f0d303bb6ac7f0eb0
Joe Mirizio <joe@joemirizio.com>
2014-06-26 02:16:55 -0400

    Add text to level select

--- src/edu/cmu/rwsefe/vowl/LevelSelectFragment.java
+++ src/edu/cmu/rwsefe/vowl/LevelSelectFragment.java
@@ -1,19 +1,57 @@
 package edu.cmu.rwsefe.vowl;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 
 public class LevelSelectFragment extends Fragment {
+	
+	public static final String LATIN_UPPER_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public static final String LATIN_LOWER_ALPHABET = LATIN_UPPER_ALPHABET.toLowerCase();
+	public static final String LATIN_NUMBERS = "0123456789";
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	    Bundle savedInstanceState) {
 	    // Inflate the layout for this fragment
-	    return inflater.inflate(R.layout.fragment_level_select, container, false);
+	    View view = inflater.inflate(R.layout.fragment_level_select, container, false);
+	    
+	    String labels = LATIN_LOWER_ALPHABET;
+	    generateLevelButtons(view, labels);
+	    
+	    return view;
+	}
+	
+	protected void generateLevelButtons(View view, String labels) {
+		String[] characters = labels.split("");
+		int characterCount = characters.length;
+		int characterIndex = 1;
+		int colorWhite = this.getResources().getColor(R.color.white);
+		TableLayout layout = (TableLayout) view.findViewById(R.id.levelTable);
+		
+		// TODO: Make level button builder dynamic
+		for (int rowIndex = 0; rowIndex < layout.getChildCount(); rowIndex++) {
+			TableRow row = (TableRow) layout.getChildAt(rowIndex);
+			for (int buttonIndex = 0; buttonIndex < row.getChildCount(); buttonIndex++) {
+				Button button = (Button) row.getChildAt(buttonIndex);
+				button.setTextSize(36);
+				button.setTextColor(colorWhite);
+				button.setText(characters[characterIndex++]);
+			}
+		}
+		
+		/*TableRow row = new TableRow(layout.getContext());
+		for(String chr : characters) {
+			if (row.getChildCount() == 3) {
+				layout.addView(row);
+				row = new TableRow(layout.getContext());
+			}
+		}*/
 	}
 }
