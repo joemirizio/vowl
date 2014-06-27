f752c9eee4e6ffe5b8110aa26847cce60483bea3
Joe Mirizio <joe@joemirizio.com>
2014-06-26 22:29:23 -0400

    Revert "Add text to level select"
    This reverts commit 582b4efcf6490d81325f4e7f0d303bb6ac7f0eb0.
    
    Conflicts:
    	src/edu/cmu/rwsefe/vowl/LevelSelectFragment.java

--- src/edu/cmu/rwsefe/vowl/LevelSelectFragment.java
+++ src/edu/cmu/rwsefe/vowl/LevelSelectFragment.java
@@ -2,35 +2,17 @@ package edu.cmu.rwsefe.vowl;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import com.canvas.*;
 
 public class LevelSelectFragment extends Fragment {
-
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-
-		View rootView = inflater.inflate(R.layout.fragment_level_select,
-				container, false);
-
-		// Start level select when nav button is clicked
-		View button = (View) rootView.findViewById(R.id.navButton);
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent();
-				intent.setClassName("com.canvas", "com.canvas.Canvas1");  //Put canvas here
-				startActivity(intent);
-			}
-		});
-
-		return rootView;
-
+	    Bundle savedInstanceState) {
+	    // Inflate the layout for this fragment
+	    return inflater.inflate(R.layout.fragment_level_select, container, false);
 	}
 }
