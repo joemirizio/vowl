cae4f598c99c0aed2b847485be8b701c99ffd917
Joe Mirizio <joe@joemirizio.com>
2014-07-07 01:19:43 -0400

    Refactor level select to use GridView and add item click listener

--- src/edu/cmu/rwsefe/vowl/LevelSelectActivity.java
+++ src/edu/cmu/rwsefe/vowl/LevelSelectActivity.java
@@ -2,15 +2,30 @@ package edu.cmu.rwsefe.vowl;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class LevelSelectActivity extends Activity {
 
+	private LevelSelectFragment mlevelSelect;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_level_select);
+		
+		mlevelSelect = (LevelSelectFragment) this.getFragmentManager().findFragmentById(R.id.level_select_fragment);
+		mlevelSelect.setLevelClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	        	String level = mlevelSelect.getLevelFromGridPosition(position);
+	        	// TODO Remove and launch canvas
+	        	Toast.makeText(LevelSelectActivity.this, "" + level, Toast.LENGTH_SHORT).show();
+	        }
+		});
 	}
 	
 	public void onRadioButtonClicked(View view) {
@@ -18,19 +33,16 @@ public class LevelSelectActivity extends Activity {
 	    boolean checked = ((RadioButton) view).isChecked();
 	    if (!checked) { return; }
 	    
-	    LevelSelectFragment levelSelect = 
-	    		(LevelSelectFragment) this.getFragmentManager().findFragmentById(R.id.level_select_fragment);
-	    
 	    // Check which radio button was clicked
 	    switch(view.getId()) {
 	        case R.id.level_alphabet_upper:
-	        	levelSelect.generateLevelButtons(levelSelect.getView(), LevelSelectFragment.LATIN_UPPER_ALPHABET);
+	        	mlevelSelect.setLevelCategory(R.string.latin_alphabet_upper);
 	            break;
 	        case R.id.level_alphabet_lower:
-	        	levelSelect.generateLevelButtons(levelSelect.getView(), LevelSelectFragment.LATIN_LOWER_ALPHABET);
+	        	mlevelSelect.setLevelCategory(R.string.latin_alphabet_lower);
 	            break;
-	        case R.id.level_number:
-	        	levelSelect.generateLevelButtons(levelSelect.getView(), LevelSelectFragment.LATIN_NUMBERS);
+	        case R.id.level_numerals:
+	        	mlevelSelect.setLevelCategory(R.string.latin_numerals);
 	            break;
 	    }
 	}
--- src/edu/cmu/rwsefe/vowl/LevelSelectFragment.java
+++ src/edu/cmu/rwsefe/vowl/LevelSelectFragment.java
@@ -3,20 +3,26 @@ package edu.cmu.rwsefe.vowl;
 import java.util.Random;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
 import edu.cmu.rwsefe.vowl.ui.FlatButtonRating;
 
 
 public class LevelSelectFragment extends Fragment {
 	
-	public static final String LATIN_UPPER_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	public static final String LATIN_LOWER_ALPHABET = LATIN_UPPER_ALPHABET.toLowerCase();
-	public static final String LATIN_NUMBERS = "0123456789";
+	final Random random = new Random();
+	
+	private LevelAdapter mLevelAdapter;
+	private GridView mLevelGridView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,52 +30,81 @@ public class LevelSelectFragment extends Fragment {
 	    // Inflate the layout for this fragment
 	    View view = inflater.inflate(R.layout.fragment_level_select, container, false);
 	    
-	    String labels = LATIN_LOWER_ALPHABET;
-	    generateLevelButtons(view, labels);
+	    String initalLevel = getActivity().getResources().getString(R.string.latin_alphabet_lower);
+	    
+	    mLevelGridView = (GridView) view.findViewById(R.id.levelGrid);
+	    mLevelAdapter = new LevelAdapter(getActivity(), initalLevel);
+	    mLevelGridView.setAdapter(mLevelAdapter);
 	    
 	    return view;
 	}
 	
-	protected void generateLevelButtons(View view, String labels) {
-		final char[] characters = labels.toCharArray();
-		final int colorWhite = this.getResources().getColor(R.color.white);
-		final int colorBlueLight = this.getResources().getColor(R.color.blueLight);
-		final int colorBlueDark = this.getResources().getColor(R.color.blueDark);
+	public void setLevelCategory(int levelCategoryId) {
+		String levels = getActivity().getResources().getString(levelCategoryId);
+		mLevelAdapter.setLevels(levels);
+	}
+	
+	public void setLevelClickListener(OnItemClickListener clickListener) {
+		mLevelGridView.setOnItemClickListener(clickListener);
+	}
+	
+	public String getLevelFromGridPosition(int position) {
+		return mLevelAdapter.getItem(position).toString();
+	}
+	
+	
+	class LevelAdapter extends BaseAdapter {
+	    private Context mContext;
+	    private String mLevels;
+	    
+	    final int colorWhite;
+	    final int colorBlueLight;
+	    final int colorBlueDark;
+	    
+	    public LevelAdapter(Context c, String levels) {
+	        mContext = c;
+	        mLevels = levels;
+	        
+	    	colorWhite = c.getResources().getColor(R.color.white);
+	    	colorBlueLight = c.getResources().getColor(R.color.blueLight);
+	    	colorBlueDark = c.getResources().getColor(R.color.blueDark);
+	    }
 
-		final int buttonsPerRow = 3;
-		final int padding = 10;
-		
-		final Random random = new Random();
-		
-		TableLayout layout = (TableLayout) view;
-		layout.removeAllViews();
-		
-		TableRow row = new TableRow(layout.getContext());
-		for(char chr : characters) {
-			// Create button
-			FlatButtonRating button = new FlatButtonRating(layout.getContext(), null);
+	    public void setLevels(String levels) {
+	    	mLevels = levels;
+	    	notifyDataSetChanged();
+	    }
+	    
+	    public int getCount() {
+	        return mLevels.length();
+	    }
+
+	    public Object getItem(int position) {
+	        return mLevels.charAt(position);
+	    }
+
+	    public long getItemId(int position) {
+	        return 0;
+	    }
+
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	    	String chr = "" + mLevels.charAt(position);
+	    	
+	    	// Create and style button
+	    	FlatButtonRating button = new FlatButtonRating(mContext, null);
 			button.setText("" + chr);
 			button.setTextSize(30);
 			button.setTextColor(colorWhite);
 			button.setBaseColor(colorBlueLight);
 			button.setShadowColor(colorBlueDark);
+			button.setHeight(300);
 			button.setRating(random.nextInt(FlatButtonRating.MAX_STARS + 1));
-			TableRow.LayoutParams buttonLayout = new TableRow.LayoutParams(0, 300);
-			buttonLayout.setMargins(padding, padding, padding, padding);
-			button.setLayoutParams(buttonLayout);
 			
-			row.addView(button);
+			// Let level select capture clicks
+			button.setClickable(false);
+			button.setFocusable(false);
 			
-			// Move to next row if current row is full
-			if (row.getChildCount() == buttonsPerRow) {
-				layout.addView(row);
-				row = new TableRow(layout.getContext());
-			}
-		}
-		
-		// Add last row if not empty
-		if (row.getChildCount() > 0) {
-			layout.addView(row);
-		}
+			return button;
+	    }
 	}
 }
