86bdd0e7c3df1bc4ef9f94548fb5361376952faa
Ambarish Karole <karoleam@gmail.com>
2014-07-28 13:36:35 -0700

    feedback text: added Try Again and Great Job

--- src/edu/cmu/rwsefe/vowl/CanvasActivity.java
+++ src/edu/cmu/rwsefe/vowl/CanvasActivity.java
@@ -31,65 +31,64 @@ public class CanvasActivity extends Activity {
 	private final String TAG = CanvasActivity.class.getName();
 	public final static String BUNDLE_LEVELS = "levels";
 	public final static String BUNDLE_LEVEL_INDEX = "levelIndex";
-	
+
 	private CanvasView mCanvasView;
 	private RatingBar mRatingBar;
 	private LevelSelector mLevelSelector;
 	private ScoreKeeper mScoreKeeper;
 	private int mConfidence;
 	private TextToSpeech mTextToSpeech;
-	
-//	ImageView image1;  //Ambarish 
-	TextView curSlidingImage; //Ambarish
-	TextView newRecord; //Ambarish
-	TextView feedbackText; //Ambarish    t.setText("done");
-	Animation animationSlideInLeft, animationSlideOutRight; //Ambarish
-	private Button prev_btn;//Ambarish
-	int old_score;//Ambarish
-	
-	
 
-	
+	// ImageView image1; //Ambarish
+	TextView curSlidingImage; // Ambarish
+	TextView newRecord; // Ambarish
+	TextView feedbackText; // Ambarish t.setText("done");
+	Animation animationSlideInLeft, animationSlideOutRight; // Ambarish
+	private Button prev_btn;// Ambarish
+	int old_score;// Ambarish
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// Restore any saved state 
+		// Restore any saved state
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.canvas);
-		
-		
-		
-//----Start---------------------------------Ambarish 
-		prev_btn = (Button)findViewById(R.id.canvasLevelNavPrev);	
-		
-		feedbackText = (TextView)findViewById(R.id.feedbackText);
-		feedbackText.setVisibility(View.GONE);// keep the image invisible on create
-		
-		newRecord = (TextView)findViewById(R.id.newRecord1);
+
+		// ----Start---------------------------------Ambarish
+		prev_btn = (Button) findViewById(R.id.canvasLevelNavPrev);
+
+		feedbackText = (TextView) findViewById(R.id.feedbackText);
+		feedbackText.setVisibility(View.GONE);// keep the image invisible on
+												// create
+
+		newRecord = (TextView) findViewById(R.id.newRecord1);
 		newRecord.setVisibility(View.GONE);// keep the image invisible on create
 
-		
-		animationSlideInLeft = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
-		animationSlideOutRight = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+		animationSlideInLeft = AnimationUtils.loadAnimation(this,
+				android.R.anim.slide_in_left);
+		animationSlideOutRight = AnimationUtils.loadAnimation(this,
+				android.R.anim.slide_out_right);
 		animationSlideInLeft.setDuration(1000);
 		animationSlideOutRight.setDuration(1000);
 		animationSlideInLeft.setAnimationListener(animationSlideInLeftListener);
-		animationSlideOutRight.setAnimationListener(animationSlideOutRightListener);
-//----End---------------------------------	
+		animationSlideOutRight
+				.setAnimationListener(animationSlideOutRightListener);
+		// ----End---------------------------------
 
-		
 		// Get character from bundle
 		Bundle bundle = getIntent().getExtras();
 		String levels = bundle.getString(BUNDLE_LEVELS);
 		int levelIndex = bundle.getInt(BUNDLE_LEVEL_INDEX, -1);
 		if (levels == null) {
-			throw new MissingResourceException("Resource is missing from bundle", 
+			throw new MissingResourceException(
+					"Resource is missing from bundle",
 					CanvasActivity.class.getName(), BUNDLE_LEVELS);
 		}
 		if (levelIndex < 0) {
-			throw new MissingResourceException("Resource is missing from bundle", 
+			throw new MissingResourceException(
+					"Resource is missing from bundle",
 					CanvasActivity.class.getName(), BUNDLE_LEVEL_INDEX);
 		}
-		
+
 		// Initialize LevelSelector and set LevelChangeListener
 		mLevelSelector = new LevelSelector(levels, levelIndex);
 		mLevelSelector.setLevelChangeListener(new LevelChangeListener() {
@@ -98,11 +97,12 @@ public class CanvasActivity extends Activity {
 				CanvasActivity.this.onLevelChange(index);
 			}
 		});
-		
+
 		mScoreKeeper = new ScoreKeeper(this);
-		
+
 		// Install LipiTK components
-		AssetInstaller assetInstaller = new AssetInstaller(getApplicationContext(), "projects");
+		AssetInstaller assetInstaller = new AssetInstaller(
+				getApplicationContext(), "projects");
 		try {
 			assetInstaller.execute();
 		} catch (IOException e) {
@@ -117,47 +117,49 @@ public class CanvasActivity extends Activity {
 				processDialogBox();
 			}
 		});
-		
+
 		mRatingBar = (RatingBar) this.findViewById(R.id.canvasRatingBar);
-		
-		mTextToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-			@Override
-			public void onInit(int status) {
-				if (status == TextToSpeech.SUCCESS) {
-					int result = mTextToSpeech.setLanguage(Locale.US);
-		            if (result == TextToSpeech.LANG_MISSING_DATA || 
-		            		result == TextToSpeech.LANG_NOT_SUPPORTED) {
-		                Log.e("TTS", "This Language is not supported");
-		            }
-			    } else { 
-			    	Log.e("TTS", "Initilization Failed!");
-			    }
-			}
-		});
-		
+
+		mTextToSpeech = new TextToSpeech(this,
+				new TextToSpeech.OnInitListener() {
+					@Override
+					public void onInit(int status) {
+						if (status == TextToSpeech.SUCCESS) {
+							int result = mTextToSpeech.setLanguage(Locale.US);
+							if (result == TextToSpeech.LANG_MISSING_DATA
+									|| result == TextToSpeech.LANG_NOT_SUPPORTED) {
+								Log.e("TTS", "This Language is not supported");
+							}
+						} else {
+							Log.e("TTS", "Initilization Failed!");
+						}
+					}
+				});
+
 		// Initialize view for level
 		onLevelChange(mLevelSelector.getLevelIndex());
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		mTextToSpeech.shutdown();
 		mScoreKeeper.close();
 		super.onDestroy();
 	}
-	
+
 	public void onClickLevelPrev(View v) {
 		mLevelSelector.prevLevel();
 	}
-	
+
 	public void onClickLevelNext(View v) {
 		mLevelSelector.nextLevel();
 	}
-	
+
 	public void onClickLevelSpeak(View v) {
-		mTextToSpeech.speak(mLevelSelector.getLevel(), TextToSpeech.QUEUE_FLUSH, null);
+		mTextToSpeech.speak(mLevelSelector.getLevel(),
+				TextToSpeech.QUEUE_FLUSH, null);
 	}
-	
+
 	public void onLevelChange(int index) {
 		mCanvasView.initializeStroke();
 		mCanvasView.setOutlineCharacter(mLevelSelector.getLevel());
@@ -166,114 +168,119 @@ public class CanvasActivity extends Activity {
 		String character = mLevelSelector.getLevel();
 		mRatingBar.setRating(mScoreKeeper.getScoreRating(character));
 	}
-	
-	public void processDialogBox(){
+
+	public void processDialogBox() {
 		ProgressdialogClass dialogBox = new ProgressdialogClass();
 		dialogBox.execute();
 	}
-	
+
 	class ProgressdialogClass extends AsyncTask<Void, Void, String> {
 		private ProgressDialog dialog;
-		
+
 		@Override
 		protected String doInBackground(Void... unsued) {
 			mCanvasView.addStroke();
 			return null;
 		}
+
 		@Override
 		protected void onPostExecute(String sResponse) {
 			dialog.dismiss();
-			
+
 			String character = mLevelSelector.getLevel();
-			HashMap<String,Integer> characters = mCanvasView.getCharacterResults();
+			HashMap<String, Integer> characters = mCanvasView
+					.getCharacterResults();
 			Integer confidenceFromHash = characters.get(character);
 			if (confidenceFromHash != null) {
 				mConfidence = confidenceFromHash;
-			}
-			else {
+			} else {
 				mConfidence = 0;
 			}
-			
+
 			int rating = mConfidence / 10;
 			mRatingBar.setRating(rating);
-			
-			if(rating > 0 ){
-		    feedbackText.setText("DONE");
-			curSlidingImage = feedbackText; //Ambarish
-			feedbackText.startAnimation(animationSlideInLeft);  //Ambarish
-			feedbackText.setVisibility(View.VISIBLE);  //Ambarish
+
+			if (rating <5) {
+				feedbackText.setText("Try Again! You Can Do Better!");
+				curSlidingImage = feedbackText; // Ambarish
+				feedbackText.startAnimation(animationSlideInLeft); // Ambarish
+				feedbackText.setVisibility(View.VISIBLE); // Ambarish
 			}
-		    
-		    
-			 prev_btn.setVisibility(View.GONE);
+			if (rating > 5) {
+				feedbackText.setText("Great Job!!");
+				curSlidingImage = feedbackText; // Ambarish
+				feedbackText.startAnimation(animationSlideInLeft); // Ambarish
+				feedbackText.setVisibility(View.VISIBLE); // Ambarish
+			}
+
+			prev_btn.setVisibility(View.GONE);
 
 			// Save confidence score to database
 			mScoreKeeper.saveScore(character, mConfidence);
-			//fetch old score
+			// fetch old score
 			old_score = mScoreKeeper.getScoreRating(character);
-			if(mConfidence>old_score){
-				newRecord.setVisibility(View.VISIBLE);// keep the image invisible on create
-				
+			if (mConfidence > old_score) {
+				newRecord.setVisibility(View.VISIBLE);// keep the image
+														// invisible on create
+
 			}
-			
+
 		}
+
 		@Override
-		protected void onPreExecute(){
-			dialog = ProgressDialog.show(CanvasActivity.this, "Processing", "Please wait...", true);
+		protected void onPreExecute() {
+			dialog = ProgressDialog.show(CanvasActivity.this, "Processing",
+					"Please wait...", true);
 		}
 	}
-	
-	//Animation-------------------------------------------------------------------- 	
-		 AnimationListener animationSlideInLeftListener
-		 = new AnimationListener(){
 
-		  @Override
-		  public void onAnimationEnd(Animation animation) {
-		   // TODO Auto-generated method stub
-		   
-		   if(curSlidingImage == feedbackText){
-		   }
- 
-		  }
+	// Animation--------------------------------------------------------------------
+	AnimationListener animationSlideInLeftListener = new AnimationListener() {
 
-		  @Override
-		  public void onAnimationRepeat(Animation animation) {
-		   // TODO Auto-generated method stub
-		   
-		  }
+		@Override
+		public void onAnimationEnd(Animation animation) {
+			// TODO Auto-generated method stub
 
-		  @Override
-		  public void onAnimationStart(Animation animation) {
-		   // TODO Auto-generated method stub
-		   
-		  }};
-		  
-		  
-		  
+			if (curSlidingImage == feedbackText) {
+			}
 
-		  AnimationListener animationSlideOutRightListener
-		  = new AnimationListener(){
-		    @Override
-		   public void onAnimationEnd(Animation animation) {
-		    // TODO Auto-generated method stub
-		     if(curSlidingImage == feedbackText){
-		    	 feedbackText.setVisibility(View.INVISIBLE);
-		   }
-		    }
+		}
 
-		   @Override
-		   public void onAnimationRepeat(Animation animation) {
-		    // TODO Auto-generated method stub
-		    
-		   }
-		   @Override
-		   public void onAnimationStart(Animation animation) {
-		    // TODO Auto-generated method stub
-		     
-		   }};
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+			// TODO Auto-generated method stub
 
-	//Animation----------------------------------------------------------------------------------
-	
-	
-	
+		}
+
+		@Override
+		public void onAnimationStart(Animation animation) {
+			// TODO Auto-generated method stub
+
+		}
+	};
+
+	AnimationListener animationSlideOutRightListener = new AnimationListener() {
+		@Override
+		public void onAnimationEnd(Animation animation) {
+			// TODO Auto-generated method stub
+			if (curSlidingImage == feedbackText) {
+				feedbackText.setVisibility(View.INVISIBLE);
+			}
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onAnimationStart(Animation animation) {
+			// TODO Auto-generated method stub
+
+		}
+	};
+
+	// Animation----------------------------------------------------------------------------------
+
 }
