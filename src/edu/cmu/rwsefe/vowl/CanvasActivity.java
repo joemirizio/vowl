2c07758287ca7c5e79cca49852795da4fe67565f
Ambarish Karole <karoleam@gmail.com>
2014-07-28 13:37:37 -0700

    feedback text: added Try Again and Great Job

--- src/edu/cmu/rwsefe/vowl/CanvasActivity.java
+++ src/edu/cmu/rwsefe/vowl/CanvasActivity.java
@@ -16,7 +16,6 @@ import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
